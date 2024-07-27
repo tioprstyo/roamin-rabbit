ab206de08078c5bddc97fe9f26175116b612f105
taufik <helloo.taufik@gmail.com>
2024-07-28 00:09:53 +0700

    update fiture 28072024

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { MainLayout } from 'components';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { LoginPage, RegisterPage, VerifikasiPage, HomePage, EsimPage, ListingPage } from 'pages';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <MainLayout />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/verifikasi" element={<VerifikasiPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/manage" element={<EsimPage />} />
+        <Route path="/listing" element={<ListingPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as LoginPage } from './login';
+export { default as RegisterPage } from './register';
+export { default as VerifikasiPage } from './verifikasi';
+export { default as HomePage } from './home';
+export { default as EsimPage } from './esim';
+export { default as ListingPage } from './listing';
